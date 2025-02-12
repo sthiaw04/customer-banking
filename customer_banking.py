df2d421597aa98bd324aa0575325b516a08f1fb1
Sokhna Thiaw <sokhna.thiaw@tfour.com>
2025-02-12 12:54:38 -0500

    last edits

--- customer_banking.py
+++ customer_banking.py
@@ -1,6 +1,7 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
-
+from savings_account import create_savings_account
+from cd_account import create_cd_account
 # Define the main function
 def main():
     """This function prompts the user to enter the savings and cd account balance, interest rate,
@@ -9,15 +10,22 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
-
+    savings_balance = float(input("Enter initial savings account balance: "))
+    savings_interest = float(input("Enter savings account annual interest rate (%): "))
+    savings_maturity = int(input("Enter savings account duration (months): "))
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f"\nSavings Account:\nInterest Earned: ${savings_interest_earned:.2f}")
+    print(f"Updated Balance: ${updated_savings_balance:.2f}")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
+    cd_balance = float(input("\nEnter initial CD account balance: "))
+    cd_interest = float(input("Enter CD account annual interest rate (%): "))
+    cd_maturity = int(input("Enter CD account duration (months): "))
 
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
@@ -27,3 +35,4 @@ def main():
 
 if __name__ == "__main__":
     # Call the main function.
+    main()
